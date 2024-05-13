43b543973e6efe141620aef4a1ae341358918db9
Esternit <92298236+Esternit@users.noreply.github.com>
2024-05-13 20:04:23 +0300

    + innertion and camera movement

--- Player.gd
+++ Player.gd
@@ -6,12 +6,20 @@ extends CharacterBody3D
 @onready var raycast = $Camera3D/RayCast3D
 @onready var walldetec = $WallDetector
 
+const BOB_FREQ = 2.0
+const BOB_AMP = 0.08
+var t_bob = 0.0 
+
 var health = 3
 var bullet = load("res://bullet.tscn")
 var instance
 var just_shot = false
-const SPEED = 7.0
+
+var speed
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 8.0 
 const JUMP_VELOCITY = 10.0
+
 var threshold_time = 0.2
 var timer = 0
 var action_started = false
@@ -71,17 +79,28 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and climbing == false:
 		velocity.y = JUMP_VELOCITY
 
+	
+	if Input.is_action_just_pressed("sprint"):
+		speed = SPRINT_SPEED
+	if Input.is_action_just_released("sprint") or speed == null:
+		speed = WALK_SPEED
 
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	if is_on_floor():
+		
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 2.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 2.0)
 	
 	if anim_player.current_animation == "shoot":
 		pass
@@ -89,6 +108,9 @@ func _physics_process(delta):
 		anim_player.play("move")
 	else:
 		anim_player.play("IdleAnimation")
+	
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = _headbob(t_bob)
 		
 	move_and_slide()
 
@@ -97,3 +119,10 @@ func play_shoot_effects():
 	anim_player.play("shoot")
 	muzzle_flash.restart()
 	muzzle_flash.emitting = true
+
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(t_bob * BOB_FREQ) * BOB_AMP + 1.225
+	pos.x = cos(t_bob * BOB_FREQ / 2) * BOB_AMP
+	return pos
+	
